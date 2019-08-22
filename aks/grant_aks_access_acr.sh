cda60b08ad5a8a4c26b5e13884b3d711d143515d
kylercai <kylerc@163.com>
2019-08-22 14:23:33 +0800

    internal lb

---
+++ aks/grant_aks_access_acr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AKS_RESOURCE_GROUP=k8sRG
+AKS_CLUSTER_NAME=ckaks
+ACR_RESOURCE_GROUP=k8sRG
+ACR_NAME=ckacr
+
+# Get the id of the service principal configured for AKS
+CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
+
+# Get the ACR registry resource id
+ACR_ID=$(az acr show --name $ACR_NAME --resource-group $ACR_RESOURCE_GROUP --query "id" --output tsv)
+
+# Create role assignment
+az role assignment create --assignee $CLIENT_ID --role acrpull --scope $ACR_ID
